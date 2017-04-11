a0cb57678ca30c081e7ac015abe7f3f752b06a6c
gao14g <gao14g@email.unc.edu>
2017-04-10 22:58:15 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ config_slack_url = 'https://hooks.slack.com/services/' # Slack integration code
 config_run_as_daemon = False
 
 ### Locales
-#config_locale = 'English_United States' # Windows locale
-config_locale = 'en_US' # Generic linux/mac locale
+config_locale = 'English_United States' # Windows locale
+#config_locale = 'en_US' # Generic linux/mac locale
 #config_locale = 'en_US.UTF-8' # Ubuntu linux locale
 #config_locale = '' # Use default locale
 ### Locales
